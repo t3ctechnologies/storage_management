b11670c389b1455ce8876d9c35dafc22a7625af2
Dayanand <dayanand.k@shrigowri.com>
2017-12-01 12:39:50 +0530

    Access key removed

--- src/main/java/com/sm/common/AccessKey.java
+++ src/main/java/com/sm/common/AccessKey.java
@@ -1,9 +1,9 @@
 package com.sm.common;
 
 public class AccessKey {
-	private static final String ACCESS_KEY = "AKIAICTRXDOYFYRUG53A";
-	private static final String SECRET_KEY = "EFlVSJMGUo7BWMC6PgcJcRjyNWwdpQmar5BdqHc3";
-	private static final String BUCKET_NAME = "weapp-storage";
+	private static final String ACCESS_KEY = " ";/*put your access key*/
+	private static final String SECRET_KEY = " ";/*put your secret key*/
+	private static final String BUCKET_NAME = " ";/*put your bucket name*/
 
 	public static String getSecretKey() {
 		return SECRET_KEY;
